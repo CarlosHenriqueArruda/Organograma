aeadc56df4e534da8962993e5c1599e97e609093
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-10-02 15:27:24 -0300

    cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador= () =>{
+  return(<div className='colaborador'>
+          <div className='cabecalho'>
+            <img src='https://github.com/CarlosHenriqueArruda.png' alt='Carlos Henrique'/>
+          </div>
+          <div className='rodape'>
+            <h4>Carlos</h4>
+            <h5>CEO</h5>
+          </div>
+        </div>
+  )
+}
+
+export default Colaborador
