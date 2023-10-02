f8e6800647f3002b186142690bf69ff1928fc5e1
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-10-02 13:17:55 -0300

    formulariofuncional

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,15 +1,54 @@
+import { useState } from "react";
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-
 const Formulario = () => {
+  const funcao = ["Selecionar...","Jungler", "Top", "Mid", "Adc", "Suport"];
+
+  const [nome,setNome] = useState('');
+  const [cargo,setCargo] = useState('');
+  const [imagem,setImagem] = useState('');
+  const [time,setTime] = useState('');
+
+  const AoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log(nome,cargo,imagem,time)
+  };
+
   return (
     <section className="formulario">
-      <form>
-        <h2>Preencha os dados para criar o Card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome..." />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..."/>
+      <form onSubmit={AoSalvar}>
+        <h2>Preencha os dados para criar o Card do jogador.</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome...*"
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Função"
+          placeholder="Digite sua Função...*"
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem..."
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Função"
+          itens={funcao}
+          valor={time}
+          aoAlterado={valor => setTime(valor)}
+          />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
