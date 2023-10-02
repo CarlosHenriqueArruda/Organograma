5fb878af6ac3e1eade58ebe37bd1a62ca6f28532
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-10-02 15:11:41 -0300

    estruturafinalizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
+import { useState } from "react";
 import Formulario from "./componentes/Formulario";
 import Banner from "./componentes/banner/banner";
+import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Back-End",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFFBD9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  const aoNovoUsuarioAdicionado = (usuario) => {
+    console.log(usuario);
+    setUsuarios([...usuarios, usuario]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario
+        times={times.map(time => time.nome)} aoUsuarioCadastrado={(usuario) => aoNovoUsuarioAdicionado(usuario)}
+      />
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,50 +4,53 @@ import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => {
-  const funcao = ["Selecionar...","Jungler", "Top", "Mid", "Adc", "Suport"];
-
-  const [nome,setNome] = useState('');
-  const [cargo,setCargo] = useState('');
-  const [imagem,setImagem] = useState('');
-  const [time,setTime] = useState('');
+const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
   const AoSalvar = (evento) => {
     evento.preventDefault();
-    console.log(nome,cargo,imagem,time)
+    props.aoUsuarioCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
     <section className="formulario">
       <form onSubmit={AoSalvar}>
-        <h2>Preencha os dados para criar o Card do jogador.</h2>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto
           obrigatorio={true}
           label="Nome"
-          placeholder="Digite seu nome...*"
+          placeholder="Digite o nome...*"
           valor={nome}
-          aoAlterado={valor => setNome(valor)}
+          aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
-          label="Função"
-          placeholder="Digite sua Função...*"
+          label="Cargo"
+          placeholder="Digite o cargo...*"
           valor={cargo}
-          aoAlterado={valor => setCargo(valor)}
+          aoAlterado={(valor) => setCargo(valor)}
         />
         <CampoTexto
           label="Imagem"
           placeholder="Digite o endereço da imagem..."
           valor={imagem}
-          aoAlterado={valor => setImagem(valor)}
+          aoAlterado={(valor) => setImagem(valor)}
         />
         <ListaSuspensa
           obrigatorio={true}
           label="Função"
-          itens={funcao}
+          itens={props.times}
           valor={time}
-          aoAlterado={valor => setTime(valor)}
-          />
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>
