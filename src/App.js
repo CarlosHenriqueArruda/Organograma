e0c5ad83428c499a55c2accc4e9ab27db635af11
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-10-02 10:26:05 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import CampoTexto from "./componentes/CampoTexto";
 import Banner from "./componentes/banner/banner";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite seu nome..." />
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..." />
     </div>
   );
 }
