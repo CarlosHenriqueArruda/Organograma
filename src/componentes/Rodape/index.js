f010bd2bfc47d5177b008b26439fd2e9d5085cfb
carloshenriquearruda <carloshenriquearruda82@gmail.com>
2023-11-27 08:45:02 -0300

    commit

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Carlos Henrique.
+        </p>
+       </section>
+    </footer>)
+}
+
+export default Rodape
